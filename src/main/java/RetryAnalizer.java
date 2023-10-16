616d6f1a888076b2ae48e4f534c7d7ee63750c5a
Milos Josifovic <josifovicmilos77@gmail.com>
2023-10-16 19:29:16 +0200

    Add RetryAnalizer class

---
+++ src/main/java/RetryAnalizer.java
@@ -0,0 +1,19 @@
+import org.testng.IRetryAnalyzer;
+import org.testng.ITestResult;
+
+public class RetryAnalizer implements IRetryAnalyzer {
+
+    private int count = 0;
+    private int maxRetry = 3;
+
+    @Override
+    public boolean retry(ITestResult iTestResult) {
+        if (count < maxRetry) {
+            count++;
+            return true;
+        }
+
+        return false;
+
+    }
+}
